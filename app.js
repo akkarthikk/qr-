2d083c3f8f500189167313c60ff04ea6fc6bb5fc
akkarthikk <159886145+akkarthikk@users.noreply.github.com>
2024-03-29 20:12:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+import express from "express";
+import qr from "qr-image";
+import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const app = express();
+const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/home.html");
+});
+app.get("/generateQRCode", (req, res) => {
+  const url = req.query.url;
+  const qr_svg = qr.image(url, { type: 'png' });
+  res.type('png');
+  qr_svg.pipe(res);
+});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
